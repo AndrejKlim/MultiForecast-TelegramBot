4b40ca1570e6ee287b57be098a540d4acb0362cb
AndrejKlim <andrei.klimovich98@mail.ru>
2022-11-13 11:13:20 +0300

    Initial commit

---
+++ src/main/java/com/multiforecast/telegrambot/MultiForecastTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.multiforecast.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiForecastTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiForecastTelegramBotApplication.class, args);
+    }
+
+}
